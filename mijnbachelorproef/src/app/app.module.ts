8f30fb5ad6673f410dac6d843d294d191c34a864
Joris Van Puyenbroeck <m9407046@student.thomasmore.be>
2023-09-11 12:30:17 +0200

    start mijn bachelorproef

--- mijnbachelorproef/src/app/app.module.ts
+++ mijnbachelorproef/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopicComponent } from './topic/topic.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopicComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
