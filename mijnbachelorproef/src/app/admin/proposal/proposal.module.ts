c6340761f0b032df2508c87740e513d16c5ba962
Joris Van Puyenbroeck <m9407046@student.thomasmore.be>
2023-09-14 13:58:58 +0200

    route guard problems

--- mijnbachelorproef/src/app/admin/proposal/proposal.module.ts
+++ mijnbachelorproef/src/app/admin/proposal/proposal.module.ts
@@ -3,11 +3,20 @@ import { SharedModule } from '../../shared/shared.module';
 import { AdminProposalListComponent } from './proposal-list/proposal-list.component';
 import { ProposalService } from './proposal.service';
 import { AdminProposalFormComponent } from './proposal-form/proposal-form.component';
+import { SecurityInterceptor } from '../../security/security.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [AdminProposalListComponent, AdminProposalFormComponent],
   imports: [SharedModule],
   exports: [AdminProposalListComponent, AdminProposalFormComponent],
-  providers: [ProposalService],
+  providers: [
+    ProposalService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SecurityInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class ProposalModule {}
--- mijnbachelorproef/src/app/app-routing.module.ts
+++ mijnbachelorproef/src/app/app-routing.module.ts
@@ -8,15 +8,23 @@ import { UserProposalListComponent } from './user/proposal/proposal-list/proposa
 import { AdminProposalListComponent } from './admin/proposal/proposal-list/proposal-list.component';
 import { AdminProposalFormComponent } from './admin/proposal/proposal-form/proposal-form.component';
 import { SecurityComponent } from './security/security/security.component';
+import { AuthGuard } from './security/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'topics', component: UserTopicListComponent },
   { path: 'proposals', component: UserProposalListComponent },
-  { path: 'admin/topic', component: AdminTopicListComponent },
-  { path: 'admin/topic/form', component: AdminTopicFormComponent },
-  { path: 'admin/proposal', component: AdminProposalListComponent },
-  { path: 'admin/proposal/form', component: AdminProposalFormComponent },
+  /*  { path: 'admin/topic', component: AdminTopicListComponent },
+    { path: 'admin/topic/form', component: AdminTopicFormComponent },
+    { path: 'admin/proposal', component: AdminProposalListComponent },
+    { path: 'admin/proposal/form', component: AdminProposalFormComponent },*/
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+  },
   { path: 'login', component: SecurityComponent },
   { path: 'register', component: SecurityComponent },
   { path: 'logout', component: SecurityComponent },
