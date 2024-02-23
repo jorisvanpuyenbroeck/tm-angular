bbf39e23c1bbf31ea75d129fa5c5a352371ef21c
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2024-02-23 17:46:30 +0100

    student application flow en stages setup, menu bar changes

---
+++ mijnbachelorproef/src/app/header/application-flow/application-flow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-flow',
+  templateUrl: './application-flow.component.html',
+  styleUrls: ['./application-flow.component.css']
+})
+export class ApplicationFlowComponent {
+
+}
