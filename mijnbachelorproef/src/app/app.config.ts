78c2337a6f379f4f37340ea696d2b15ebd91eb90
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2024-03-10 08:42:51 +0100

    back to local

--- mijnbachelorproef/src/app/app.config.ts
+++ mijnbachelorproef/src/app/app.config.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
     providedIn: 'root', // Make sure it's available globally
 })
 export class ApiConfigService {
-    apiBaseUrl = 'https://bpgezinswetenschappenapi.azurewebsites.net/api/'; // Development URL
-    // apiBaseUrl = 'https://localhost:7026/api/'; // Development URL
+    // apiBaseUrl = 'https://bpgezinswetenschappenapi.azurewebsites.net/api/'; // Development URL
+    apiBaseUrl = 'https://localhost:7026/api/'; // Development URL
 
 
     // You can add other configuration properties here
--- mijnbachelorproef/src/environments/environment.prod.ts
+++ mijnbachelorproef/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // environment.ts
 export const environment = {
-  production: true, // this is true in the production environment
+  production: false, // this is true in the production environment
   AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
   AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
   AUTH0_AUDIENCE: "https://bpgezinswetenschappenapi.azurewebsites.net",
